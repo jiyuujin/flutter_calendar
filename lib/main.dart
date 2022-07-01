956484b22344b0d46a0c4c53e14000fd763e5b06
chooyan-eng <chooyan.engineer@gmail.com>
2022-07-01 18:16:48 +0900

    book の内容にあわせて修正

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:calendar_logic/calendar_logic.dart';
+import 'package:calendar_widget/calendar_widget.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -6,111 +6,113 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
-  // This widget is the root of your application.
+  // 自動生成されたコメントは全て削除
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Calendar', // アプリのタイトルを修正
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const CalendarPage(), // home 引数に指定する Widget を CalendarPage に変更
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+// MyHomePage を CalendarPage に変更（StatefulWidget のまま）
+class CalendarPage extends StatefulWidget {
+  const CalendarPage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<CalendarPage> createState() => _CalendarPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+// _MyHomePageState を _CalendarPageState に変更
+class _CalendarPageState extends State<CalendarPage> {
+  // カウンター用のプロパティとメソッドは全て削除
 
-  void _incrementCounter() {
+  var _currentDate = DateTime.now();
+
+  void _toNextMonth() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter = Calculator().addOne(_counter);
+      _currentDate = DateTime(_currentDate.year, _currentDate.month + 1);
+    });
+  }
+
+  void _toPreviousMonth() {
+    setState(() {
+      _currentDate = DateTime(_currentDate.year, _currentDate.month - 1);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      appBar: AppBar(title: const Text('Calendar')),
+      body: SizedBox.expand(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            children: [
+              _CalendarController(
+                currentMonth: '${_currentDate.year}年 ${_currentDate.month}月',
+                onNextTap: _toNextMonth,
+                onPreviousTap: _toPreviousMonth,
+              ),
+              Calendar(date: _currentDate),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class _CalendarController extends StatelessWidget {
+  const _CalendarController({
+    required this.currentMonth,
+    required this.onPreviousTap,
+    required this.onNextTap,
+  });
+
+  /// 現在の年月として表示する文字列
+  final String currentMonth;
+
+  /// 「前の月へ」アイコンがタップされた場合のコールバック
+  final VoidCallback onPreviousTap;
+
+  /// 「次の月へ」アイコンがタップされた場合のコールバック
+  final VoidCallback onNextTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        // 「前の月へ」アイコン
+        InkWell(
+          onTap: onPreviousTap,
+          child: const Padding(
+            padding: EdgeInsets.all(8),
+            child: Icon(Icons.navigate_before),
+          ),
+        ),
+        // 現在表示中の年月
+        Expanded(
+          child: Center(
+            child: Text(currentMonth),
+          ),
+        ),
+        // 「次の月へ」アイコン
+        InkWell(
+          onTap: onNextTap,
+          child: const Padding(
+            padding: EdgeInsets.all(8),
+            child: Icon(Icons.navigate_next),
+          ),
+        ),
+      ],
     );
   }
 }
